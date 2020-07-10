d34d8588a693a56a0104d4e2ab253fb215913d9a
jacobwicks <jacob.m.wicks@gmail.com>
2020-07-10 11:55:27 -0700

    Updates

---
+++ precache-manifest.afcb390feb233a00bace7707ff464b61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a195985d694a36ce523c5ce2ed85b4",
+    "url": "/instructions/index.html"
+  },
+  {
+    "revision": "447c5223464e215de592",
+    "url": "/instructions/static/css/main.f0baed30.chunk.css"
+  },
+  {
+    "revision": "bc82115053852d653131",
+    "url": "/instructions/static/js/2.2952a1d5.chunk.js"
+  },
+  {
+    "revision": "f0ab5801985173a52868f196793396c8",
+    "url": "/instructions/static/js/2.2952a1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447c5223464e215de592",
+    "url": "/instructions/static/js/main.4cd13e98.chunk.js"
+  },
+  {
+    "revision": "9d31913fe0301bc557c1",
+    "url": "/instructions/static/js/runtime-main.07fb6c5d.js"
+  }
+]);
